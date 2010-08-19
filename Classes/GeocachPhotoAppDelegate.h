192838546e66c8e1ebbce6932913a33016af5c58
Tnicholas <tnicholas@divergentlogic.com>
2010-08-19 15:38:58 -0600

    first commit

---
+++ Classes/GeocachPhotoAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  GeocachPhotoAppDelegate.h
+//  GeocachPhoto
+//
+//  Created by Ty Nicholas on 8/19/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface GeocachPhotoAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
